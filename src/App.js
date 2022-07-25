966fefdf6c63b008e2b805d46fbb20b8c5d28583
Mikhail Maliar <mike@drupfan.com>
2022-07-25 17:36:29 +0300

    kk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import styled from 'styled-components'
+import {AnimatePresence, motion} from "framer-motion";
+const initialBrands = ["jcb", "visa", "mastercard", "discover", "amex"];
 
+const assetsPath = 'img/'
+const CardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+
+  height: 26px;
+
+
+`;
 function App() {
+  const [search, setSearch] = useState('')
+  const [availableCardBrands ,setAvailableCardBrands] = useState(initialBrands)
+  useEffect(() => {
+    if(!search){
+      setAvailableCardBrands(initialBrands)
+      return
+    }
+    const userCardBrand = initialBrands.filter(
+      (brand) => brand.includes(search)
+    );
+
+    setAvailableCardBrands(userCardBrand);
+  }, [search]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input className="aba" onChange={(e) => setSearch(e.target.value)}/>
+      <CardWrapper as={motion.div} >
+        <AnimatePresence>
+          {availableCardBrands.map(brand => (
+            <motion.img
+              key={brand}
+              layout
+              src={`${assetsPath}icon-payment-${brand}.svg`}
+              alt={`${brand} card icon`}
+              animate={{opacity: 1}} initial={{opacity: 0}} exit={{opacity: 0}} transition={{ease: "easeOut"}}
+            />
+          ))}
+        </AnimatePresence>
+      </CardWrapper>
     </div>
   );
 }
